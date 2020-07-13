bffee805df1d7117273b0bec19ed7be1be79a3a7
Amy Churchwell <achurchwell@mozilla.com>
2020-07-12 19:47:59 -0700

    Update CMS config

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,18 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-  />
-)
+const AboutPagePreview = ({ entry, getAsset }) => {
+  const entryItems = entry.getIn(['data', 'intro', 'blurbs'])
+  const blurbs = entryItems ? entryItems.toJS() : []
+
+  return (
+    <AboutPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      intro={{ blurbs }}
+    />
+  )
+}
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  widgetFor: PropTypes.func,
 }
 
 export default AboutPagePreview
